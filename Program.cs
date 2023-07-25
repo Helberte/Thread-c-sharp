e4af2b741eb325992cf8815a6f58c866d6a9fefa
Helberte Costa Arruda <helberteads@gmail.com>
2023-07-25 18:04:18 -0400

    algoritmo pesado

--- Program.cs
+++ Program.cs
@@ -2,4 +2,25 @@
 
 
 
+TarefaLonga tarefaLonga = new();
 
+tarefaLonga.LoopGrande();
+
+
+
+
+
+
+public class TarefaLonga
+{
+    long teste = 0;
+
+    public void LoopGrande()
+    {
+        for (int i = 0; i < 9999999999999999999999F; i++)
+        {
+            for (int j = 0; j < 99999999999999m; j++)            
+                teste += j;            
+        }
+    }
+}
